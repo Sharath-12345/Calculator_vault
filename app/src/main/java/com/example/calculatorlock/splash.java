6eef1b8cc4bf9b1b166a679a62f6d92b13cf14e2
sharath <sharathkohli777@gmail.com>
2023-08-14 04:43:51 -0700

    this is my first commit

---
+++ app/src/main/java/com/example/calculatorlock/splash.java
@@ -0,0 +1,33 @@
+package com.example.calculatorlock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView imageView=findViewById(R.id.splashimage);
+        imageView.setScaleY(1.7f);
+        imageView.setScaleX(1.7f);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2500);
+
+
+    }
+
+}
